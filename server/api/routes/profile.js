47f3b4be9f9166fbb6c02882ef420aae44d8acde
ahmed hassan <eng.ahmedhassan.eng@gmail.com>
2019-02-23 03:12:32 -0800

    fix package json

--- server/api/routes/profile.js
+++ server/api/routes/profile.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var jwt = require('express-jwt');
-var auth = jwt({
-  secret: 'MY_SECRET',
-  userProperty: 'payload'
-});
 var ctrlProfile = require('../controllers/profile');
 // profile
-router.get('/profile', auth, ctrlProfile.profileRead);
+router.get('/profile', ctrlProfile.profileRead);
 
 module.exports = router;
 
--- server/api/routes/security.js
+++ server/api/routes/security.js
@@ -1,10 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var jwt = require('express-jwt');
-var auth = jwt({
-  secret: 'MY_SECRET',
-  userProperty: 'payload'
-});
 
 var ctrlAuth =  require('../controllers/authentication');
 
