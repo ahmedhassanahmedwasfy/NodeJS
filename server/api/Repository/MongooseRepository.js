50f25a72662d0ced005fd835d1c980ca3bef7b95
ahmed hassan <eng.ahmedhassan.eng@gmail.com>
2019-03-10 09:12:33 -0700

    finish group

--- server/api/Repository/MongooseRepository.js
+++ server/api/Repository/MongooseRepository.js
@@ -4,6 +4,7 @@ class MongooseRepository {
     constructor(collectionName){
         this.collection = require('../models/'+collectionName)
     }
+
     async findOne(query, populate){
         let data,err;
         let q = this.collection.findOne(query)
@@ -20,6 +21,7 @@ class MongooseRepository {
         // let {err,res}= await this.collection.find().lean().exec();
        return {err,data};
     }
+
     async find(query,skip,limit,populate){
         let data,err;
         try {
@@ -41,6 +43,7 @@ class MongooseRepository {
         // let {err,res}= await this.collection.find().lean().exec();
         return {err,data};
     }
+
     async delete(query){
         let data,err;
         try {
@@ -53,10 +56,11 @@ class MongooseRepository {
         // let {err,res}= await this.collection.find().lean().exec();
         return {err,data};
     }
-    async update(query){
+
+    async update(model){
         let data,err;
         try {
-            data=   await this.collection.updateOne(query).lean().exec();
+            data=   await this.collection.findOneAndUpdate({_id:model._id},model,{upsert: true, 'new': true}).lean().exec();
 
         }catch (e) {
             err=e;
@@ -65,6 +69,7 @@ class MongooseRepository {
         // let {err,res}= await this.collection.find().lean().exec();
         return {err,data};
     }
+
     async create(objs){
         let data,err;
         try {
@@ -80,6 +85,5 @@ class MongooseRepository {
         return {err,data};
     }
 }
-
 
 module.exports=MongooseRepository;
--- server/api/routes/security.js
+++ server/api/routes/security.js
@@ -5,6 +5,8 @@ const mongoose=require('mongoose');
 var ctrlAuth =  require('../controllers/authentication');
 var ctrlPermission =  require('../controllers/permission');
 var ctrlGroup =  require('../controllers/group');
+var ctrlUser =  require('../controllers/user');
+
 
 // authentication
 router.post('/register', function(req,res){
@@ -22,18 +24,18 @@ router.get('/permissions', function(req,res){
     ctrlPermission.index(req,res)
 });
 
-router.get('/permissions/view/:permission_id', function(req,res){
+router.get('/permissions/view/:id', function(req,res){
     ctrlPermission.view(req,res)
 });
 
-router.post('/permissions/add', function(req,res){
+router.post('/permissions/create', function(req,res){
         ctrlPermission.new(req,res)
     });
 
-router.post('/permissions/update/:permission_id', function (req, res) {
+router.post('/permissions/update', function (req, res) {
     ctrlPermission.update(req,res)});
 
-router.delete('/permissions/delete/:permission_id', function (req, res) {
+router.delete('/permissions/delete/:id', function (req, res) {
     ctrlPermission.delete(req,res)});
 
 
@@ -43,25 +45,29 @@ router.get('/groups', function(req,res){
         ctrlGroup.index(req,res)
     });
 
-router.get('/groups/group_id', function (req, res) {
+router.get('/groups/view/:id', function (req, res) {
     ctrlGroup.view(req, res)
 
 });
 
-router.post('/groups', function(req,res){
+router.post('/groups/create', function(req,res){
         ctrlGroup.new(req,res)
     });
 
-router.post('/groups/group_id', function(req,res){
+router.post('/groups/update', function(req,res){
     ctrlGroup.update(req, res)
 });
 
-router.post('/groups/group_id', function (req, res) {
+router.delete('/groups/delete/:id', function (req, res) {
     ctrlGroup.delete(req, res)
 });
 
 
+///////////GroUsersups////////////
 
+router.get('/users', function(req,res){
+    ctrlUser.index(req,res)
+});
 
 
 module.exports = router;
